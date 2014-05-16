b381764efe53e94289651a9a4850ae53103cd990
Paul Liu <Paul@deep-thought.(none)>
2014-05-16 19:08:30 -0400

    git ignore added

--- send_probs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-./gen_html_uva.py | email -html -s "Daily UVA" "paul.liu.ubc@gmail.com, supreme.overlord.of.lala.land@gmail.com, jonathanasdf@gmail.com, dllu@dllu.net, anguslim94@gmail.com, jsonchiu@gmail.com"
